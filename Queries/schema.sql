f7156eb812de08d7275428de0d5049b61861cda9
MStewart0218 <https://github.com/MStewart0218/Election_Analysis.git>
2022-03-12 17:35:49 -0600

    Updates

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -33,11 +33,11 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 );
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
@@ -47,4 +47,5 @@ CREATE TABLE titles (
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
-SELECT * FROM departments;
+
+SELECT * FROM 	titles;
