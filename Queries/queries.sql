5da4237312ff8eb3a32cf2dd816db31e474ced93
MStewart0218 <https://github.com/MStewart0218/Election_Analysis.git>
2022-03-13 16:01:49 -0500

    Updated queries and added current_emp table

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -3,29 +3,67 @@ SELECT first_name, last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- Retirment eligibility table
+
+-- Retirement eligibility table
 SELECT first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 SELECT * FROM retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the retire,ment_info table
+SELECT * FROM retirement_info;
+
+-- Drop retirement_info table
+DROP TABLE retirement_info;
+
 -- Number of employees retiring
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- Retirement eligibilty for employees birth years 1952, 1953, 1954, and 1955
+
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
 SELECT * FROM 	titles;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
